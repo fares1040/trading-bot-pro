52b4ab60da0b852124356130f8d8b6d23d188a80
fares1040 <ff2016ff2017ff2018@gmail.com>
2026-07-16 07:22:03 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,7 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="ar" dir="rtl">
-      <body style={{ backgroundColor: '#000', color: '#fff', margin: 0, fontFamily: 'sans-serif' }}>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
